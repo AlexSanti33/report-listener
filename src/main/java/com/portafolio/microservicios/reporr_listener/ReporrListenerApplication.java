eed6b7cb4810fe24c09b97e8e367a98334139e89
AlexSanti33 <santiago.jav27@gmail.com>
2024-06-01 01:29:19 -0600

    first commit

---
+++ src/main/java/com/portafolio/microservicios/reporr_listener/ReporrListenerApplication.java
@@ -0,0 +1,13 @@
+package com.portafolio.microservicios.reporr_listener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReporrListenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReporrListenerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portafolio/microservicios/reporr_listener/ReporrListenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portafolio.microservicios.reporr_listener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReporrListenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
